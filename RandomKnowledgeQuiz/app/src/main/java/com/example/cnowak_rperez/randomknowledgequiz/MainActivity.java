3546b551153903a2f4ded757ec4d4e2eb2693feb
cnowak7 <rafi.slapass@gmail.com>
2015-11-20 18:51:12 -0600

    fixed small bug

--- RandomKnowledgeQuiz/app/src/main/java/com/example/cnowak_rperez/randomknowledgequiz/MainActivity.java
+++ RandomKnowledgeQuiz/app/src/main/java/com/example/cnowak_rperez/randomknowledgequiz/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.cnowak_rperez.randomknowledgequiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Button startQuiz;
 
